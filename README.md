41b09c0669cad0c7b8a18d274bf57a9dace24f85
Mehmet Cenk Yenikoylu <mcyenikoylu@gmail.com>
2020-03-30 16:22:06 +0300

    tahmin

--- README.md
+++ README.md
@@ -15,3 +15,10 @@ payment_type,
 fare_amount
 
 mlnet auto-train --task multiclass-classification --dataset "taxi-fare-train.csv" --label-column-name "rate_code" --max-exploration-time 10
+
+cd consumeModelApp
+dotnet add reference ../SampleMulticlassClassification/SampleMulticlassClassification.Model/
+
+dotnet add package Microsoft.ML --version 1.4.0
+
+SampleMulticlassClassification.Model klasorundeki 'MLModel.zip' dosyasini kopyalayip consumeModelApp klasorune yapistirin.
--- consumeModelApp/Program.cs
+++ consumeModelApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using SampleMulticlassClassification.Model.DataModels;
+using Microsoft.ML;
 
 namespace consumeModelApp
 {
@@ -6,7 +8,44 @@ namespace consumeModelApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            ConsumeModel();
+        }
+        public static void ConsumeModel()
+        {
+            // Load the model
+            MLContext mlContext = new MLContext();
+
+            ITransformer mlModel = mlContext.Model.Load("MLModel.zip", out var modelInputSchema);
+
+            var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+
+            // Use the code below to add input data
+            var input = new ModelInput();
+            //input.SentimentText = "That is rude";
+
+// vendor_id,
+// rate_code,
+// passenger count,
+// trip time in secs,
+// trip distance,
+// payment_type,
+// fare_amount
+            //CMT,3,1,2528,18.1,CSH,71
+            
+            input.Rate_code = 3;
+    //        input.Passenger_count = 1;
+//            input.Trip_time_in_secs = 2528;
+//input.Trip_distance = float.Parse("18.1");
+input.Payment_type = "CSH";
+//input.Fare_amount = 71;
+
+            // Try model on sample data
+            // True is toxic, false is non-toxic
+            ModelOutput result = predEngine.Predict(input);
+
+            //Console.WriteLine($"satıcı kimliği: {input.Vendor_id} | Oran Kodu: {input.Rate_code} | Yolcu Sayisi: {input.Passenger_count} | saniye olarak yolculuk süresi: {input.Trip_time_in_secs} | yolculuk mesafesi: {input.Trip_distance} | ödeme şekli: {input.Payment_type} | Hesap toplamı: {input.Fare_amount}");
+        Console.WriteLine($"Tahmin: {(Convert.ToBoolean(result.Prediction) ? "Olumlu" : "Olumsuz")} yolcu");
         }
     }
 }
